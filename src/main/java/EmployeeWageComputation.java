58b62ef92d8635388a9c20c79dfedf61588f39ff
amitatiwari255 <amita.tiwari255@gmail.com>
2022-11-23 15:08:42 +0530

    Added:Program to calculate Employee PartTime and FullTime Wage

--- src/main/java/EmployeeWageComputation.java
+++ src/main/java/EmployeeWageComputation.java
@@ -1,4 +1,4 @@
-
+// UC-1
 public class EmployeeWageComputation  {
 	void checkEmployeeAttendence() {
 	 	   int IS_FULL_TIME = 1;
@@ -9,29 +9,49 @@ public class EmployeeWageComputation  {
 	 		   System.out.println("Employee is Absent");
 
 	    }
+	//UC-2
 	//Daily Employee Wage
 	void checkDailyWages() {
 		int IsFullTime=1;
-	    int IsPartTime=2;
 	    int EmployeeRatePerHr=20;
 	    int FulldayHr=8;
-	    int HalfdayHr=4;
 	    
 	    double employeeCheck = Math.floor(Math.random() * 10) %2;
 		if ( employeeCheck == IsFullTime ) {
 			System.out.println( "EmployeeFulldaywage = " + ( FulldayHr * EmployeeRatePerHr));
 		}
 			else {
-				System.out.println ("EmployeeHalfdaywage=" + (HalfdayHr *EmployeeRatePerHr));
+				System.out.println ("Employeewage=" + (0));
 			}
-		
 	}
-	public static void main(String[] args) {
-		EmployeeWageComputation  Emp = new EmployeeWageComputation ();
-		Emp.checkDailyWages();
-	}
+
+	//public static void main(String[] args) {
+	//	EmployeeWageComputation  Emp = new EmployeeWageComputation ();
+		//Emp.checkDailyWages();
+	
 //	public static void main(String[] args) {		EmployeeWageComputation  Emp = new EmployeeWageComputation ();
 		
+//UC-3
+void checkPartimeandFulltimeWages() {
+	int IsFullTime = 1;
+    int IsPartTime= 2 ;
+    int EmployeeRatePerHr= 20;
+    int empHrs= 0;
+    int empWage= 0;
+    
+    double employeeCheck = Math.floor(Math.random() * 10) %3;
+	if ( employeeCheck == IsFullTime ) {
+		empHrs= 8;
+	}
+		else if ( employeeCheck == IsPartTime){		
+			empHrs=4;
+		}
+	empWage = empHrs * EmployeeRatePerHr;
+System.out.println("empWage : =" + empWage);
 }
-	
-//
+
+public static void main(String[] args) {
+	EmployeeWageComputation  Emp = new EmployeeWageComputation ();
+		Emp.checkPartimeandFulltimeWages();
+}
+}
